e41c08d7f9b3e4102b2918c094b7c3c66cfe89dd
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-02-06 13:31:05 -0500

    added state variable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+];
+
+export default function App() {
+    const [step, setStep] = useState(1)
+
+    // Previous Button Function
+    function handlePrevious() {
+        if(step > 1) {
+            setStep(step - 1)
+        }
+    }
+
+    // Next Button Function
+    function handleNext() {
+        if(step < 3) {
+            setStep(step + 1)
+        }
+    }
+
+    return <div className='steps'>
+        
+        <div className='numbers'>
+            <div className={step >=1 ? "active" : ""}>1</div>
+            <div className={step >=2 ? "active" : ""}>2</div>
+            <div className={step >=3 ? "active" : ""}>3</div>
+        </div>
+
+        <p className='message'>
+            Step {step}: {messages[step - 1]}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+        <div className='buttons'>
+            <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handlePrevious}>
+                Previous
+            </button>
+            <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handleNext}>
+                Next
+            </button>
+        </div>
+
+    </div>
+}
